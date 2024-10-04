4def1e5cff3c8e514c7b70433ec543790f02532c
Tommy Bozeman <tboz203@gmail.com>
2024-10-04 10:47:22 -0500

    streaming example works now

--- streaming_example.py
+++ streaming_example.py
@@ -4,64 +4,50 @@
 Shows how to use the Converse API to stream a response from Anthropic Claude 3 Sonnet (on demand).
 """
 
+from __future__ import annotations
+
 import logging
 import boto3
-
+from typing import TYPE_CHECKING
 
 from botocore.exceptions import ClientError
 
+if TYPE_CHECKING:
+    from mypy_boto3_bedrock_runtime import type_defs as brtd
+    from botocore.eventstream import EventStream
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("streaming_example")
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s.%(msecs)03d %(levelname)8s %(name)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# MODEL_ID = "anthropic.claude-3-sonnet-20240229-v1:0"
+# MODEL_ID = "amazon.titan-text-express-v1"
+MODEL_ID = "mistral.mistral-small-2402-v1:0"
 
 
-def stream_conversation(bedrock_client, model_id, messages, system_prompts, inference_config, additional_model_fields):
-    """
-    Sends messages to a model and streams the response.
-    Args:
-        bedrock_client: The Boto3 Bedrock runtime client.
-        model_id (str): The model ID to use.
-        messages (JSON) : The messages to send.
-        system_prompts (JSON) : The system prompts to send.
-        inference_config (JSON) : The inference configuration to use.
-        additional_model_fields (JSON) : Additional model fields to use.
+def stream_conversation(stream: EventStream[brtd.ConverseStreamResponseTypeDef]) -> None:
+    for event in stream:
+        if "messageStart" in event:
+            print(f"\nRole: {event['messageStart']['role']}")
 
-    Returns:
-        Nothing.
+        if "contentBlockDelta" in event:
+            print(event["contentBlockDelta"]["delta"]["text"], end="")
 
-    """
+        if "messageStop" in event:
+            print(f"\nStop reason: {event['messageStop']['stopReason']}")
 
-    logger.info("Streaming messages with model %s", model_id)
-
-    response = bedrock_client.converse_stream(
-        modelId=model_id,
-        messages=messages,
-        system=system_prompts,
-        inferenceConfig=inference_config,
-        additionalModelRequestFields=additional_model_fields,
-    )
-
-    stream = response.get("stream")
-    if stream:
-        for event in stream:
-            if "messageStart" in event:
-                print(f"\nRole: {event['messageStart']['role']}")
-
-            if "contentBlockDelta" in event:
-                print(event["contentBlockDelta"]["delta"]["text"], end="")
-
-            if "messageStop" in event:
-                print(f"\nStop reason: {event['messageStop']['stopReason']}")
-
-            if "metadata" in event:
-                metadata = event["metadata"]
-                if "usage" in metadata:
-                    print("\nToken usage")
-                    print(f"Input tokens: {metadata['usage']['inputTokens']}")
-                    print(f":Output tokens: {metadata['usage']['outputTokens']}")
-                    print(f":Total tokens: {metadata['usage']['totalTokens']}")
-                if "metrics" in event["metadata"]:
-                    print(f"Latency: {metadata['metrics']['latencyMs']} milliseconds")
+        if "metadata" in event:
+            metadata = event["metadata"]
+            if "usage" in metadata:
+                print("\nToken usage")
+                print(f"Input tokens: {metadata['usage']['inputTokens']}")
+                print(f":Output tokens: {metadata['usage']['outputTokens']}")
+                print(f":Total tokens: {metadata['usage']['totalTokens']}")
+            if "metrics" in event["metadata"]:
+                print(f"Latency: {metadata['metrics']['latencyMs']} milliseconds")
 
 
 def main():
@@ -69,35 +55,41 @@ def main():
     Entrypoint for streaming message API response example.
     """
 
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-
-    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
-    system_prompt = """You are an app that creates playlists for a radio station
-      that plays rock and pop music. Only return song names and the artist."""
+    system_prompt = (
+        "You are an app that creates playlists for a radio station that plays "
+        "rock and pop music. Only return song names and the artist."
+    )
 
     # Message to send to the model.
     input_text = "Create a list of 3 pop songs."
 
-    message = {"role": "user", "content": [{"text": input_text}]}
+    message = {
+        "role": "user",
+        "content": [
+            {
+                "text": input_text,
+            }
+        ],
+    }
     messages = [message]
-
-    # System prompts.
-    system_prompts = [{"text": system_prompt}]
-
-    # inference parameters to use.
-    temperature = 0.5
-    top_k = 200
-    # Base inference parameters.
-    inference_config = {"temperature": temperature}
-    # Additional model inference parameters.
-    additional_model_fields = {"top_k": top_k}
+    system_prompts = [
+        {
+            "text": system_prompt,
+        }
+    ]
 
     try:
+        logger.info("Streaming messages with model %s", MODEL_ID)
+
         bedrock_client = boto3.client(service_name="bedrock-runtime")
 
-        stream_conversation(
-            bedrock_client, model_id, messages, system_prompts, inference_config, additional_model_fields
+        response = bedrock_client.converse_stream(
+            modelId=MODEL_ID,
+            messages=messages,
+            system=system_prompts,
         )
+
+        stream_conversation(response["stream"])
 
     except ClientError as err:
         message = err.response["Error"]["Message"]
@@ -105,7 +97,7 @@ def main():
         print("A client error occured: " + format(message))
 
     else:
-        print(f"Finished streaming messages with model {model_id}.")
+        print(f"Finished streaming messages with model {MODEL_ID}.")
 
 
 if __name__ == "__main__":
